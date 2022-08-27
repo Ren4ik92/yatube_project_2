c91bff6c1a152fc698fc7254ee6ee86ab0948f2e
Belotserkovets Mikhail <ren4ik92@gmail.com>
2022-08-27 11:03:03 +0300

    r

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube.'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube.'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
