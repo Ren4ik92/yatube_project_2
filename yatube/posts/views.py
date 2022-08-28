0c105ddc39bb3a5dd590cdc53ca36ea44270dbe8
Belotserkovets Mikhail <ren4ik92@gmail.com>
2022-08-28 14:08:30 +0300

    90%

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 # posts/views.py
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
     template = 'posts/index.html'
@@ -12,9 +12,11 @@ def index(request):
 
 
 def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
     template = 'posts/group_list.html'
-    text = 'sdfsdfsdf'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': text
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
